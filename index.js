44ae1d667f6f4ba9a6a363e0fee1a2d5455cd617
Sakib8git <sakib30278@gnail.com>
2025-12-12 16:50:11 +0600

    deploy done

--- index.js
+++ index.js
@@ -12,14 +12,10 @@ const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
 );
 const serviceAccount = JSON.parse(decoded);
 
-// const serviceAccount = require(process.env.FIREBASE_ADMIN_KEY_PATH);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-// const serviceAccount = JSON.parse(decoded);
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+
 
 const app = express();
 // middleware
